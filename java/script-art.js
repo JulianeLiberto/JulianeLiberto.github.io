a4b4618719cabceea5f156ef2307a35fbd69ce79
JulianeLiberto <89662291+JulianeLiberto@users.noreply.github.com>
2026-07-23 18:14:56 -0400

    Update script-art.js

--- java/script-art.js
+++ java/script-art.js
@@ -2,7 +2,7 @@ const lightbox = document.createElement('div');
 lightbox.id = 'lightbox';
 document.body.appendChild(lightbox);
 
-const images = document.querySelectorAll('img');
+const images = document.querySelectorAll('#photos img, #pottery img');
 
 images.forEach(image => {
   image.addEventListener('click', e => {
